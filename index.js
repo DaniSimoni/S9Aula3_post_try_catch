9f6fb9b97dc7e8d219c253c202b6ab8baa3fece6
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-03-30 21:39:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const connection = require('./src/database');
+const Task = require('./src/models/task');
+
+
+const app = express();
+app.listen(3333, () => console.log("Aplicação Online Lindamente ^^ S2"));
+
+
+app.get('/', (req, res) => {
+    console.log("Cheguei aqui!")
+    res.json({mensagem: "Vem chegando!"})
+});
+
+app.use(express.json());
+
+/* const tarefas = []; */
+
+connection.authenticate()
+connection.sync()
+console.log('Okkkkkkkkkkkkkkk')
+
+
+
+app.post('/tarefas', async (req, res) => {
+
+    try {
+    const tarefa = {
+        name: req.body.name,
+        description: req.body.description
+    }
+
+    if (!tarefa.name) {
+        return res.status(401).json({message: 'Nome é Obrigatório'});
+    } 
+
+    if (!tarefa.description) {
+        return res.status(401).json({message: 'Descrição é Obrigatório'});
+    } 
+
+    const newTask = await Task.create(tarefa)
+
+    res.status(201).json(newTask); 
+
+    } catch (error) {
+        res.status(500).json({message: "Não foi possível processar sua solicitação"})
+        console.log('Errou')
+    }
+    
+
+   
+});
+
+
+app.get('/tarefas', (req, res) => {
+    res.json(tasks.find(id(2)));
+    console.log(tarefa)
+});
+
+
+
+
+
